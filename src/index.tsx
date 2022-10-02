5b1f145f60894c24a8347c92bed5396f3a3e46bc
Ice Nitipon <nitipon.t@techcombine.co>
2022-10-02 18:02:19 +0700

    [🚀] setup firebase hosting

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import firebase from "./firebase";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
